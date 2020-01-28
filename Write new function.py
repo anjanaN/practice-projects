396db1fee24bf5bf01d5be2e9dd095863258794f
Anjana Nair <anjananair@LiveSafes-MacBook-Pro-9.local>
2020-01-28 14:47:59 -0500

    adding files

---
+++ Write new function.py
@@ -0,0 +1,7 @@
+print('Hello', end='')
+print('World')
+
+print('cat', 'dog', 'mouse')
+print('cat', 'dog', 'mouse', sep='ABC')
+
+
---
+++ plusOne function.py
@@ -0,0 +1,7 @@
+def plus_one(number):
+    return number + 1
+
+
+newNumber = plus_one(5)
+
+print(newNumber)
